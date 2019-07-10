a359db708e085057feb0dbec97672537902aeeb0
Oleksiy Protas <elfy@ecognize.me>
2019-07-10 17:29:44 +0300

    Middleware settings switch, suggested by pytest-django
    https://django.cowhite.com/blog/django-middleware-the-new-version-of-middleware-and-how-to-use-it/

--- backend/promis/promis/settings.py
+++ backend/promis/promis/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
 
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
